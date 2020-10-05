3cb5078b13f406ee558964713566bc6fba28cfb1
stream <stream2jazz@gmail.com>
2020-10-05 20:30:24 +0800

    book Pagination - use js

--- src/main/java/com/imooc/reader/controller/BookController.java
+++ src/main/java/com/imooc/reader/controller/BookController.java
@@ -1,9 +1,13 @@
 package com.imooc.reader.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.imooc.entity.Book;
 import com.imooc.entity.Category;
+import com.imooc.service.BookService;
 import com.imooc.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -14,13 +18,29 @@ public class BookController {
 
     @Resource
     private CategoryService categoryService;
+    @Resource
+    private BookService bookService;
 
-    @GetMapping("/")
+    @GetMapping("/Book")
     public ModelAndView showIndex(){
         ModelAndView mav = new ModelAndView("/index");
         List<Category> categoryList = categoryService.selectAll();
         mav.addObject("categoryList",categoryList);
         return mav;
-
     }
+
+    /**
+     *分頁查詢圖書列表
+      * @param p 頁號
+     * @return 分頁對象
+     */
+    @GetMapping("/books")
+    @ResponseBody
+     public IPage<Book> selectBook(Integer p){
+        if(p==null){
+            p = 1;
+        }
+         IPage<Book> pageObjeck = bookService.paging(p, 10);
+        return  pageObjeck;
+     }
 }
